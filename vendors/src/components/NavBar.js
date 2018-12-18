e5ffa9ebd0c74f2d41b6b7c79e2366e80d967129
respicio.robert <respicio.robert@gmail.com>
2018-12-18 13:24:13 -0800

    check in

--- vendors/src/components/NavBar.js
+++ vendors/src/components/NavBar.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Link } from 'react-router-dom';
+//import { Route, Link } from 'react-router-dom';
 import Login from './Login';
 import Register from './Register';
 
--- vendors/src/components/Register.js
+++ vendors/src/components/Register.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import TextInput from '../common/TextInput';
 
 class Register extends React.Component {
   constructor(props) {
